69e791d6bc3acb5726eaff89ed3c245cc6913aa7
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-15 23:27:29 -0400

    [feat]: req 4 feito no choro

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,17 +41,20 @@ def scrape_next_page_link(html_content):
 def scrape_news(html_content):
     """Seu código deve vir aqui"""
     selecionado = Selector(text=html_content)
-    return (
-        {
-            "url": selecionado.css(),
-            "title": selecionado.css(),
-            "timestamp": selecionado.css(),
-            "writer": selecionado.css(),
-            "reading_time": selecionado.css(),
-            "summary": selecionado.css(),
-            "category": selecionado.css(),
-        }
-    )
+    scrape = {
+        "url": selecionado.css("head link[rel='canonical']::attr(href)").get(),
+        "title": selecionado.css('.entry-title::text').get().strip(),
+        "timestamp": selecionado.css('.meta-date::text').re_first(r"\d{2}/\d{2}/\d{4}"),
+        "writer": selecionado.css('.author a::text').get(),
+        "reading_time": int(selecionado.css(".meta-reading-time::text").get().split(" ")[0]),
+        "summary": selecionado
+        .xpath("string(//div[@class='entry-content']/p[1])")
+        .getall()[0]
+        .strip(),
+        "category": selecionado.css('.category-style .label::text').get(),
+    }
+    # print('print do texto >>>>>', scrape)
+    return scrape
 
 
 # Requisito 5
