efdea5fd0d531ef818bdbb2b4c7450c84e0b506e
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2023-02-16 12:39:21 -0400

    [feat]: req 7 e 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,29 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    retorno = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [
+        (news["title"], news["url"]) for news in retorno
+    ]
 
 
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    # print(date)
+    # https://www.programiz.com/python-programming/datetime/strftime
+    try:
+        changed_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        retorno_db = search_news({"timestamp": changed_date})
+        return [
+            (news["title"], news["url"]) for news in retorno_db
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
